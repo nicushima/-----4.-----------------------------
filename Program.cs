245c2968e0e01fbcb55cde43e8e84789bbe033db
nicu <gg.nicu>
2023-10-27 17:38:42 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// int Factorial(int n)
+// {
+//     if (n == 1) return 1;
+//     else return n * Factorial(n - 1);
+// }
+// Console.WriteLine(Factorial(3));
+
+
+
+
+// числа фибаначи 
+ int Fibonachi(int n)
+ {
+    int (n == 1 || n == 2) return 1;
+    else Fibonachi (n-1) + Fibonachi(n-2); 
+ }
+
+ for (int i = 1; i < 10; i++)
+ {
+    Console.WriteLine(Fibonachi(i));
+ }
